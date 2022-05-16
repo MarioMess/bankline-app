e91645196e58fdf2f8e18bff22348eabde05f04c
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2022-05-16 09:44:48 -0300

    moving URL to folder environment

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-const baseUrl = 'http://localhost:8080';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +11,10 @@ export class CorrentistaService {
   constructor(private http: HttpClient) { }
 
   list(): Observable<any> {
-    return this.http.get(`${baseUrl}/correntistas`);
+    return this.http.get(`${environment.baseUrl}/correntistas`);
   }
 
   create(correntista: any): Observable<any> {
-    return this.http.post(`${baseUrl}/correntistas`, correntista);
+    return this.http.post(`${environment.baseUrl}/correntistas`, correntista);
   }
 }
--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-const baseUrl = 'http://localhost:8080';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +11,14 @@ export class MovimentacaoService {
   constructor(private http: HttpClient) { }
 
   list(): Observable <any> {
-    return this.http.get(`${baseUrl}/movimentacoes`);
+    return this.http.get(`${environment.baseUrl}/movimentacoes`);
   }
 
   findByIdConta(idConta:any): Observable<any> {
-    return this.http.get(`${baseUrl}/movimentacoes/${idConta}`);
+    return this.http.get(`${environment.baseUrl}/movimentacoes/${idConta}`);
   }
   // adicionando o método de inclusão (POST) via API
 create(movimentacao:any): Observable<any> {
-  return this.http.post(`${baseUrl}/movimentacoes`,movimentacao);
+  return this.http.post(`${environment.baseUrl}/movimentacoes`,movimentacao);
 }
 }
